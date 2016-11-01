3066f688b68bea2e17268470d76615bf115e2c63
Izza <icg417@gmail.com>
2016-11-01 15:55:04 -0400

    added additional columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+	has_attached_file :avatar,
+ 	:styles => { :medium => "300x300>", :thumb => "200x200>" },
+ 	:default_url => "/images/missing.png"
+
+	validates_attachment_content_type :avatar,
+ 	:content_type => /\Aimage\/.*\Z/
+ 	
 end
