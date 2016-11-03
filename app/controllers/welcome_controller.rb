3d4020e89e1ba42d861f71747b08c83223f9f504
Brian Altman <balt490@gmail.com>
2016-11-03 17:46:33 -0400

    fixed merge problems, added in sign up sign in and logout and job and shipper status

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   	@user = current_user
+
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20161103185417) do
     t.string   "username"
     t.string   "dob"
     t.string   "gender"
+    t.string   "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
