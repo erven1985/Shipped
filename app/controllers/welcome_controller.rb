075ea90e365ac97e6e9c0e08a825d0d95bf2eff9
Yerlan <bayekeshov@gmail.com>
2016-11-03 14:13:03 -0400

    Profile Page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+  	@user = current_user
+  	
   end
+
+  def show
+  	@user = current_user
+  	params[:id] = current_user.id 
+  end
+
 end
